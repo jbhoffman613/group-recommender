c528ae11cf6ca498eaa69153d3936c80301ca8a7
setteducatis <setteducati.s@husky.neu.edu>
2019-04-06 16:31:10 -0400

    Added csv and csv read function

--- generator.py
+++ generator.py
@@ -4,6 +4,7 @@ from . import config
 connection = pymysql.connect(host='localhost',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor, **config.MYSQL)
+csv_name = "Group Recommender - Data Collection (Responses) - Form Responses 1.csv"
 
 try:
     with connection.cursor() as cursor:
@@ -12,3 +13,11 @@ try:
             cursor.execute(query)
 finally:
     connection.close()
+    
+def read_csv(csv_file_name):
+    with open(csv_file_name, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in spamreader:
+            print(', '.join(row))
+
+read_csv(csv_name)
