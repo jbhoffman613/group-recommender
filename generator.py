a81a60fc56645704784bc197bb6cc1abb7de5b4c
Michael Fleischmann <mfleisch50@gmail.com>
2019-04-06 16:26:39 -0400

    Creation

--- generator.py
+++ generator.py
@@ -1,8 +1,13 @@
-import pymysql, config
+import pymysql, config, csv
 
 connection = pymysql.connect(host='localhost',
                              charset='utf8mb4',
                              cursorclass=pymysql.cursors.DictCursor, **config.MYSQL)
 
-
-
+try:
+    with connection.cursor() as cursor:
+        creation = ["drop database if exists grs", "create database grs"]
+        for query in creation:
+            cursor.execute(query)
+finally:
+    connection.close()
